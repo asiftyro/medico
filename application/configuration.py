f317b91730a3f00f64258ac57e864cfff6c751b5
asiftyro <asif.tyro@gmail.com>
2023-01-02 00:15:18 +0600

    config for mysql

--- application/configuration.py
+++ application/configuration.py
@@ -6,63 +6,65 @@ load_dotenv()
 
 
 class BaseConfiguration:
-  # Application
-  DEBUG = False
-  APP_NAME = os.getenv('APP_NAME', 'App Name')
-  APP_VER = os.getenv('APP_VER', 'x.y.z')
-  APP_DIR = os.path.abspath(os.path.dirname(__file__))
-  ROOT_DIR = os.path.abspath(os.path.join(APP_DIR, os.pardir))
-  # Static assets
-  STATIC_DIR_PATH = os.path.join(APP_DIR, os.getenv('STATIC_DIR_PATH', 'static'))
-  USER_AVATAR_DIR = os.path.join(STATIC_DIR_PATH, 'img', "user-avatar")
-  _app_dir = APP_DIR[:-1] if APP_DIR[-1] == '/' else APP_DIR  # Strip trailing slash
-  _static_dir = STATIC_DIR_PATH[1:] if STATIC_DIR_PATH[0] == '/' else STATIC_DIR_PATH  # Strip trailing slash
-  _static_dir = _static_dir[:-1] if _static_dir[-1] == '/' else _static_dir  # Strip starting slash
-  USER_AVATAR_DIR = _app_dir + "/" + _static_dir + '/img/user-avatar'
-  CASE_PHOTO_DIR = _app_dir + "/" + _static_dir + '/img/case-photo'
-  # Session
-  SECRET_KEY = os.getenv('SECRET_KEY', '32_bit_long_random_secret_string')
-  SESSION_COOKIE_HTTPONLY = True
-  REMEMBER_COOKIE_HTTPONLY = True
-  # SQLAlchemy
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
-  SQLITE_DB_NAME = os.getenv('SQLITE_DB_NAME', 'db.sqlite3')
-  SQLITE_PATH = 'sqlite:///' + os.path.join(ROOT_DIR, SQLITE_DB_NAME)
-  SQLALCHEMY_DATABASE_URI = SQLITE_PATH
-  # Content-Encoding/Compression
-  COMPRESS_ALGORITHM = 'gzip'
-  COMPRESS_REGISTER = True
-  # Local Timezome
-  LOCAL_TIMEZONE = os.getenv('LOCAL_TIMEZONE', 'Asia/Dhaka')
-  # Bootstrap
-  BOOTSTRAP_SERVE_LOCAL = True
-  BOOTSTRAP_BTN_STYLE = 'dark'
-  # BOOTSTRAP_BOOTSWATCH_THEME='cerulean'
+    # Application
+    DEBUG = False
+    APP_NAME = os.getenv("APP_NAME", "App Name")
+    APP_VER = os.getenv("APP_VER", "x.y.z")
+    APP_DIR = os.path.abspath(os.path.dirname(__file__))
+    ROOT_DIR = os.path.abspath(os.path.join(APP_DIR, os.pardir))
+    # Static assets
+    STATIC_DIR_PATH = os.path.join(APP_DIR, os.getenv("STATIC_DIR_PATH", "static"))
+    USER_AVATAR_DIR = os.path.join(STATIC_DIR_PATH, "img", "user-avatar")
+    _app_dir = APP_DIR[:-1] if APP_DIR[-1] == "/" else APP_DIR  # Strip trailing slash
+    _static_dir = STATIC_DIR_PATH[1:] if STATIC_DIR_PATH[0] == "/" else STATIC_DIR_PATH  # Strip trailing slash
+    _static_dir = _static_dir[:-1] if _static_dir[-1] == "/" else _static_dir  # Strip starting slash
+    USER_AVATAR_DIR = _app_dir + "/" + _static_dir + "/img/user-avatar"
+    CASE_PHOTO_DIR = _app_dir + "/" + _static_dir + "/img/case-photo"
+    # Session
+    SECRET_KEY = os.getenv("SECRET_KEY", "32_bit_long_random_secret_string")
+    SESSION_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_HTTPONLY = True
+    # SQLAlchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLITE_DB_NAME = os.getenv("SQLITE_DB_NAME", "db.sqlite3")
+    SQLITE_PATH = "sqlite:///" + os.path.join(ROOT_DIR, SQLITE_DB_NAME)
+    SQLALCHEMY_DATABASE_URI = SQLITE_PATH
+    # Content-Encoding/Compression
+    COMPRESS_ALGORITHM = "gzip"
+    COMPRESS_REGISTER = True
+    # Local Timezome
+    LOCAL_TIMEZONE = os.getenv("LOCAL_TIMEZONE", "Asia/Dhaka")
+    # Bootstrap
+    BOOTSTRAP_SERVE_LOCAL = True
+    BOOTSTRAP_BTN_STYLE = "dark"
+    # BOOTSTRAP_BOOTSWATCH_THEME='cerulean'
 
 
 class DevelopmentConfiguration(BaseConfiguration):
-  # Application
-  DEBUG = True
-  APP_NAME = f"{BaseConfiguration.APP_NAME} {{δ}}"
-  # Template debugging
-  TEMPLATES_AUTO_RELOAD = True
-  EXPLAIN_TEMPLATE_LOADING = False
-  # Session
-  SESSION_COOKIE_HTTPONLY = False
-  REMEMBER_COOKIE_HTTPONLY = False
-  REMEMBER_COOKIE_DURATION = 60 * 60 * 24 * 1  # 1 days
-  # SQLAlchemy
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Application
+    DEBUG = True
+    APP_NAME = f"{BaseConfiguration.APP_NAME} {{δ}}"
+    # Template debugging
+    TEMPLATES_AUTO_RELOAD = True
+    EXPLAIN_TEMPLATE_LOADING = False
+    # Session
+    SESSION_COOKIE_HTTPONLY = False
+    REMEMBER_COOKIE_HTTPONLY = False
+    REMEMBER_COOKIE_DURATION = 60 * 60 * 24 * 1  # 1 days
+    # SQLAlchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfiguration(BaseConfiguration):
-  DEBUG = False
-  # Session
-  REMEMBER_COOKIE_DURATION = 60 * 60 * 24 * 7  # 7 days
-  # MySql
-  # SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(os.getenv('DB_ENGINE', 'mysql'),
-  #                                                        os.getenv('DB_USERNAME', 'mysql_db_user'),
-  #                                                        os.getenv('DB_PASSWORD', 'mysql_db_user'),
-  #                                                        os.getenv('DB_HOST', 'mysql_db_host'),
-  #                                                        os.getenv('DB_PORT', 3306),
-  #                                                        os.getenv('DB_NAME', 'mysql_db_name'))
+    DEBUG = False
+    # Session
+    REMEMBER_COOKIE_DURATION = 60 * 60 * 24 * 7  # 7 days
+    # MySql
+    SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}:{}/{}".format(
+        os.getenv("DB_ENGINE", "mysql"),
+        os.getenv("DB_USERNAME", "mysql_db_user"),
+        os.getenv("DB_PASSWORD", "mysql_db_user"),
+        os.getenv("DB_HOST", "mysql_db_host"),
+        os.getenv("DB_PORT", 3306),
+        os.getenv("DB_NAME", "mysql_db_name"),
+    )
---
+++ mysql_schema/mysql_schema.sql
@@ -0,0 +1,110 @@
+
+USE MEDICO;
+SET FOREIGN_KEY_CHECKS=0;
+
+-- Table: user
+
+
+DROP TABLE IF EXISTS user;
+
+
+
+CREATE TABLE user (
+	id INTEGER NOT NULL AUTO_INCREMENT, 
+	username VARCHAR(16) NOT NULL, 
+	password_hash VARCHAR(128), 
+	fullname VARCHAR(64) NOT NULL, 
+	dob DATE NOT NULL, 
+	sex VARCHAR(1) NOT NULL, 
+	blood VARCHAR(3), 
+	reference VARCHAR(64), 
+	email VARCHAR(64), 
+	address VARCHAR(64), 
+	admin SMALLINT NOT NULL, 
+	active SMALLINT NOT NULL, 
+	author INTEGER, 
+	created_at DATETIME, 
+	modified_at DATETIME, 
+	avatar VARCHAR(128), analysis TEXT, case_photo_1 VARCHAR(128), case_photo_2 VARCHAR(128), case_photo_3 VARCHAR(128), case_photo_4 VARCHAR(128), 
+	CONSTRAINT pk_user PRIMARY KEY (id), 
+	CONSTRAINT uq_user_username UNIQUE (username), 
+	CONSTRAINT uq_user_email UNIQUE (email), 
+	CONSTRAINT fk_user_author_user FOREIGN KEY(author) REFERENCES user (id)
+);
+
+
+-- Table: prescription
+DROP TABLE IF EXISTS prescription;
+CREATE TABLE prescription (
+	id INTEGER NOT NULL AUTO_INCREMENT, 
+	note TEXT, 
+	prescription TEXT, 
+	patient_id INTEGER, 
+	author INTEGER, 
+	created_at DATETIME, 
+	modified_at DATETIME, 
+	follow_up_date DATE, 
+	CONSTRAINT pk_prescription PRIMARY KEY (id), 
+	CONSTRAINT fk_prescription_author_user FOREIGN KEY(author) REFERENCES user (id), 
+	CONSTRAINT fk_prescription_patient_id_user FOREIGN KEY(patient_id) REFERENCES user (id)
+);
+
+
+
+-- Table: conversation
+DROP TABLE IF EXISTS conversation;
+CREATE TABLE conversation (
+	id INTEGER NOT NULL AUTO_INCREMENT, 
+	conversation VARCHAR(128), 
+	`read` SMALLINT, 
+	patient_id INTEGER, 
+	author INTEGER, 
+	created_at DATETIME, 
+	modified_at DATETIME, 
+	admin_id INTEGER, 
+	CONSTRAINT pk_conversation PRIMARY KEY (id), 
+	CONSTRAINT fk_conversation_author_user FOREIGN KEY(author) REFERENCES user (id), 
+	CONSTRAINT fk_conversation_patient_id_user FOREIGN KEY(patient_id) REFERENCES user (id), 
+	CONSTRAINT fk_conversation_admin_id_user FOREIGN KEY(admin_id) REFERENCES user (id)
+);
+
+-- Table: medicine
+DROP TABLE IF EXISTS medicine;
+CREATE TABLE medicine (
+	id INTEGER NOT NULL AUTO_INCREMENT, 
+	short_name VARCHAR(128), 
+	medicine VARCHAR(128), 
+	author INTEGER, 
+	created_at DATETIME, 
+	modified_at DATETIME, 
+	CONSTRAINT pk_medicine PRIMARY KEY (id), 
+	CONSTRAINT fk_medicine_author_user FOREIGN KEY(author) REFERENCES user (id), 
+	CONSTRAINT uq_medicine_medicine UNIQUE (medicine), 
+	CONSTRAINT uq_medicine_short_name UNIQUE (short_name)
+);
+
+
+INSERT INTO medico.`user`
+(
+username, 
+password_hash,
+fullname, 
+dob, 
+sex, 
+blood, 
+admin, 
+active, 
+author
+)
+VALUES
+(
+'root', 
+'sha256$9BKqwnG8UDTSVJHc$d46b6bde118980d83b6b538448e948ed41c60aee5ddd30368cfe7ac20a82d875', 
+'Sys Admin', 
+'1983-12-31', 
+'M', 
+'B+',
+1, 
+1, 
+1
+);
