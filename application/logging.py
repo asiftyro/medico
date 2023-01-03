d2e5fa621179cdd7942c76d9ebb4731130c6f30b
asiftyro <asif.tyro@gmail.com>
2023-01-04 00:49:27 +0600

    Switch for loading logging schema in dot env

--- application/logging.py
+++ application/logging.py
@@ -1,6 +1,13 @@
 import logging
+from dotenv import load_dotenv
+import os
 from logging.config import dictConfig
 from application.configuration import BaseConfiguration
 
-dictConfig(BaseConfiguration.LOG_SCHEMA)
+load_dotenv()
+
+enable_logger = int(os.getenv("ENABLE_LOGGER_CONFIG", '1').strip())
+if enable_logger:
+    dictConfig(BaseConfiguration.LOG_SCHEMA)
 logger = logging.getLogger(__name__)
+
