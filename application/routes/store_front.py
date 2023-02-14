d6541d53699b1e08ac11732a8d5d05ada0befd0b
asiftyro <asif.tyro@gmail.com>
2023-02-14 22:10:02 +0600

    set-store-front-url

--- application/routes/store_front.py
+++ application/routes/store_front.py
@@ -8,4 +8,4 @@ blueprint = Blueprint("store_front_bp", __name__, url_prefix="/")
 # Public route, open for all
 @blueprint.route("/", methods=["GET"])
 def index():
-    return render_template("store_front/index.html")
+    return redirect("http://homeopathbd.xyz/")
